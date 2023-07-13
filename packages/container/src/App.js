dccde1a41793beea998511084fa1a25b9e34ab5f
Saeid Barzegar <saeidbarzegar@gmail.com>
2023-07-13 14:07:37 +0800

    prevent css class collision added

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -2,14 +2,22 @@ import React from 'react'
 import MarketingApp from "./components/MarketingApp";
 import { BrowserRouter } from "react-router-dom";
 import Header from './components/Header';
+import { StylesProvider, createGenerateClassName } from "@material-ui/core";
+
+const generateClassName = createGenerateClassName({
+  productionPrefix: "con", 
+});
+
 export default () => {
   
   return(
-    <BrowserRouter>
-      <div>
-        <Header />
-        <MarketingApp />
-      </div>
-    </BrowserRouter>
+    <StylesProvider>
+      <BrowserRouter generateClassName={generateClassName}>
+        <div>
+          <Header />
+          <MarketingApp />
+        </div>
+      </BrowserRouter>
+    </StylesProvider>
   )
 };
--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -4,13 +4,21 @@ import { Switch, Route, BrowserRouter } from "react-router-dom"
 
 import Landing from "./components/Landing";
 import Pricing from "./components/Pricing";
-import { StylesProvider } from '@material-ui/core';
+import { StylesProvider, createGenerateClassName } from '@material-ui/core';
 
+/**
+ * to prevent classname collission 
+ * because of using same ui framework 
+ * on both container and remote apps
+ */
+const generateClassName = createGenerateClassName({
+  productionPrefix: "ma", // will add ma prefix to all classes
+});
 
 export default () => {
   return(
     <div>
-      <StylesProvider>
+      <StylesProvider generateClassName={generateClassName}>
         <BrowserRouter>
           <Switch>
             <Route path="/pricing" component={Pricing} exact/>
